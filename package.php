5deb5d430d833734b614c383b85928bbfaa5f1fc
Kayila <arekuanubis@gmail.com>
2016-07-07 23:11:33 -0400

    Initial commit

---
+++ package.php
@@ -0,0 +1,11 @@
+<?php
+
+$packages[] = array(
+    'name' => 'Elisium110',
+    'title' => 'Elisium110',
+    'version' => 'VERSION',
+    'priority' => 0,
+    'location' => 'modpacks/elisium110.json',
+);
+
+?>
